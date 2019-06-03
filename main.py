0bf4deb4572b90871cacf4ff4dbc2510093eb803
Russell Neville <russell.neville@cambiahealth.com>
2019-06-03 14:03:14 -0700

    enabled the flask app to ru; config changes to connect to datastore; implemented model_datastore methods to write to datastore

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import socket
 import config
 
 import model_datastore
+from google.cloud import datastore
 
 # [START imports]
 from flask import Flask, render_template, request
@@ -22,6 +23,7 @@ app.testing = False
 # Configure logging
 if not app.testing:
     logging.basicConfig(level=logging.INFO)
+
 
 
 # [START default route]
@@ -40,23 +42,18 @@ def form():
 # [START submitted]
 @app.route('/submitted', methods=['POST'])
 def submitted_form():
-
- #   ds = datastore.Client()
+    
 
     name = request.form['name']
     email = request.form['email']
     participant_type = request.form['participant_type']
     timestamp = datetime.datetime.utcnow()
 
-    # entity = datastore.Entity(key=ds.key('Registration'))
-    # entity.update({
-    #     'user_name': name,
-    #     'email': email,
-    #     'participant_type': participant_type,
-    #     'timestamp': timestamp
-    # })
-
-    # ds.put(entity)
+    data = request.form.to_dict(flat=True)
+    data['timestamp'] = unicode(timestamp)
+    app.logger.info(data)    
+ 
+    model_datastore.update(data)
 
 
     # [END submitted]
@@ -76,3 +73,6 @@ def server_error(e):
     logging.exception('An error occurred during a request.')
     return 'An internal error occurred.', 500
 # [END app]
+
+if __name__ == "__main__":
+    app.run()
